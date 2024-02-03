49930f7fc8765d256cbbd398f646af9a54675a76
Midhun <midhunskani@gmail.com>
2024-02-03 08:54:20 +0530

    CAF-4--Create users model

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,6 +3,5 @@ from . import views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("trainer_dashboard/", TemplateView.as_view(template_name="html/trainer_dashboard.html"), name='trainer_dashboard'),
-
+    path("trainer_dashboard/", TemplateView.as_view(template_name="dashboard.html"), name='trainer_dashboard'),
 ]
