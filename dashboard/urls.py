feed3e36a67f8c30455555e4da9b06c716b2c022
Midhun <midhunskani@gmail.com>
2024-02-04 15:01:21 +0530

    CAF-10--Create the upload Receipe page

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,4 +4,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path("trainer_dashboard/", TemplateView.as_view(template_name="dashboard.html"), name='trainer_dashboard'),
+    path("upload_recipe/", TemplateView.as_view(template_name="html/upload_recipe.html"), name='upload_recipe'),
 ]
