4bfbda38ae55725dbc7a29792a9a7ed21d860e27
Midhun <midhunskani@gmail.com>
2024-02-07 16:01:58 +0530

    CAF-16--Show the nav links based on user groups

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path("trainer_dashboard/", TemplateView.as_view(template_name="dashboard.html"), name='trainer_dashboard'),
-    path("upload_recipe/", TemplateView.as_view(template_name="html/upload_recipe.html"), name='upload_recipe'),
-    path("recipe_list/", TemplateView.as_view(template_name="html/recipe_list.html"), name='recipe_list'),
-    path("bank_statement/", TemplateView.as_view(template_name="html/bank_statement.html"), name='bank_statement'),
+    path("trainer_dashboard/", TemplateView.as_view(template_name="trainer_dashboard/trainer_dashboard.html"), name='trainer_dashboard'),
+    path("upload_recipe/", TemplateView.as_view(template_name="trainer_dashboard/upload_recipe.html"), name='upload_recipe'),
+    path("recipe_list/", TemplateView.as_view(template_name="trainer_dashboard/recipe_list.html"), name='recipe_list'),
+    path("bank_statement/", TemplateView.as_view(template_name="trainer_dashboard/bank_statement.html"), name='bank_statement'),
 ]
