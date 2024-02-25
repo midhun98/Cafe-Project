039317190bf0cf295104c05977cbb7f48917f3bc
Midhun <midhunskani@gmail.com>
2024-02-25 13:29:25 +0530

    CAF-9--Modify the settings and urls for uploading images

--- cafeProject/urls.py
+++ cafeProject/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('', include('dashboard.urls')),
-
 ]
+urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})]
