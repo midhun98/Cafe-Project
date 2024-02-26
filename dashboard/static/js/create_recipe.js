2bd5a22ea571ce93cc59749d19506a16e78c6af8
Midhun <midhunskani@gmail.com>
2024-02-26 22:18:15 +0530

    CAF-25--Create the api to create food by the trainer

--- dashboard/static/js/create_recipe.js
+++ dashboard/static/js/create_recipe.js
@@ -2,6 +2,104 @@ window.onload = function () {
     document.getElementById("msform").reset();
 };
 
+const nextButton = document.getElementById('nextButton');
+
+function checkFields() {
+    var recipeName = document.getElementById('recipeName').value;
+    var calories = document.getElementById('calories').value;
+    var protein = document.getElementById('protein').value;
+    var fat = document.getElementById('fat').value;
+    var carbs = document.getElementById('carbs').value;
+
+    if (recipeName !== '' && calories !== '' && protein !== '' && fat !== '' && carbs !== '') {
+        document.getElementById('nextButton').removeAttribute('disabled');
+    } else {
+        document.getElementById('nextButton').setAttribute('disabled', 'disabled');
+    }
+}
+
+// Add event listeners to all input fields
+
+function validateForm() {
+    let isValid = true;
+    const recipeName = document.getElementById('recipeName').value.trim();
+    const calories = document.getElementById('calories').value.trim();
+    const protein = document.getElementById('protein').value.trim();
+    const fat = document.getElementById('fat').value.trim();
+    const carbs = document.getElementById('carbs').value.trim();
+    if (!recipeName) {
+        document.getElementById('nextButton').setAttribute('disabled', 'disabled');
+        document.getElementById('recipeNameError').innerText = 'Recipe Name is required';
+        document.getElementById('recipeNameError').style.display = 'block';
+        isValid = false;
+    } else {
+        console.log('else recipeNameError')
+        document.getElementById('nextButton').removeAttribute('disabled');
+        document.getElementById('recipeNameError').style.display = 'none';
+    }
+
+    if (!calories) {
+        document.getElementById('nextButton').setAttribute('disabled', 'disabled');
+        document.getElementById('caloriesError').innerText = 'Calories is required';
+        document.getElementById('caloriesError').style.display = 'block';
+        isValid = false;
+    } else {
+        document.getElementById('nextButton').removeAttribute('disabled');
+        document.getElementById('caloriesError').style.display = 'none';
+    }
+
+    if (!protein) {
+        document.getElementById('nextButton').setAttribute('disabled', 'disabled');
+        document.getElementById('proteinError').innerText = 'Protein is required';
+        document.getElementById('proteinError').style.display = 'block';
+        isValid = false;
+    } else {
+        document.getElementById('nextButton').removeAttribute('disabled');
+        document.getElementById('proteinError').style.display = 'none';
+    }
+
+    if (!fat) {
+        document.getElementById('nextButton').setAttribute('disabled', 'disabled');
+        document.getElementById('fatError').innerText = 'Fat is required';
+        document.getElementById('fatError').style.display = 'block';
+        isValid = false;
+    } else {
+        document.getElementById('nextButton').removeAttribute('disabled');
+        document.getElementById('fatError').style.display = 'none';
+    }
+
+    if (!carbs) {
+        document.getElementById('nextButton').setAttribute('disabled', 'disabled');
+        document.getElementById('carbsError').innerText = 'Carbs is required';
+        document.getElementById('carbsError').style.display = 'block';
+        isValid = false;
+    } else {
+        document.getElementById('nextButton').removeAttribute('disabled');
+        document.getElementById('carbsError').style.display = 'none';
+    }
+
+    return isValid;
+}
+
+document.getElementById('recipeName').addEventListener('input', validateForm);
+document.getElementById('calories').addEventListener('input', validateForm);
+document.getElementById('protein').addEventListener('input', validateForm);
+document.getElementById('fat').addEventListener('input', validateForm);
+document.getElementById('carbs').addEventListener('input', validateForm);
+
+function nextStep() {
+    const isValid = validateForm();
+    console.log('isValid', isValid)
+    if (isValid) {
+        console.log('Form is valid');
+        nextButton.removeAttribute('disabled'); // Enable button
+    } else {
+        console.log('Form contains errors');
+        nextButton.setAttribute('disabled', 'disabled');
+    }
+}
+
+
 $(document).ready(function () {
     $("#msform").submit(function (event) {
         console.log("hii");
